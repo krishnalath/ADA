6475e431b8a847a93c27975d2e227dfda1f4497a
krishnalath <krishnalath.cs24@bmsce.ac.in>
2026-05-25 09:30:29 +0530

    Add files via upload

---
+++ topological.c
@@ -0,0 +1,94 @@
+
+Kritika Panwar
+9:22 AM (0 minutes ago)
+to me
+
+#include <stdio.h>
+
+#define MAX 100
+
+int graph[MAX][MAX];
+int indegree[MAX];
+int queue[MAX];
+int front = 0, rear = 0;
+
+void topologicalSort(int n)
+{
+    int i, j, count = 0;
+
+    // Calculate indegree of each vertex
+    for(i = 0; i < n; i++)
+    {
+        indegree[i] = 0;
+    }
+
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            if(graph[i][j] == 1)
+            {
+                indegree[j]++;
+            }
+        }
+    }
+
+    // Insert vertices with indegree 0 into queue
+    for(i = 0; i < n; i++)
+    {
+        if(indegree[i] == 0)
+        {
+            queue[rear++] = i;
+        }
+    }
+
+    printf("Topological Ordering: ");
+
+    while(front < rear)
+    {
+        int vertex = queue[front++];
+        printf("%d ", vertex);
+        count++;
+
+        // Reduce indegree of adjacent vertices
+        for(i = 0; i < n; i++)
+        {
+            if(graph[vertex][i] == 1)
+            {
+                indegree[i]--;
+
+                if(indegree[i] == 0)
+                {
+                    queue[rear++] = i;
+                }
+            }
+        }
+    }
+
+    // Check for cycle
+    if(count != n)
+    {
+        printf("\nGraph contains a cycle. Topological ordering not possible.");
+    }
+}
+
+int main()
+{
+    int n, i, j;
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix:\n");
+
+    for(i = 0; i < n; i++)
+    {
+        for(j = 0; j < n; j++)
+        {
+            scanf("%d", &graph[i][j]);
+        }
+    }
+
+    topologicalSort(n);
+
+    return 0;
