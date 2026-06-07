dcb6695badf654a5e5c0a2aa093db4c8367264a8
krishnalath <krishnalath.cs24@bmsce.ac.in>
2026-06-07 16:23:11 +0530

    Update N_Queens.c

--- N_Queens.c
+++ N_Queens.c
@@ -1,50 +1,35 @@
+// Online C compiler to run C program online
 #include <stdio.h>
-#include <stdlib.h>
-
-int pos[20];
-
-int safe(int row, int col)
-{
-    for(int i = 0; i < row; i++)
-    {
-        if(pos[i] == col ||
-           abs(pos[i] - col) == abs(i - row))
+#include<stdlib.h>
+int pos[20],count=0;
+int isSafe(int row,int col){
+    for(int i=0;i<row;i++){
+        if(pos[i]==col || abs(pos[i]-col)==abs(i-row))
             return 0;
     }
     return 1;
 }
-
-void solve(int row, int n)
-{
-    if(row == n)
-    {
-        printf("\nSolution:\n");
-
-        for(int i = 0; i < n; i++)
-            printf("Row %d -> Column %d\n",
-                    i+1, pos[i]+1);
-
+void solve(int row,int n){
+    if(row==n){
+        count++;
+        printf("Solution %d\n",count);
+        for(int i=0;i<n;i++)
+            printf("Row %d -> Col %d\n",i+1,pos[i]+1);
         return;
     }
-
-    for(int col = 0; col < n; col++)
-    {
-        if(safe(row,col))
-        {
-            pos[row] = col;
+    for(int col=0;col<n;col++){
+        if(isSafe(row,col)){
+            pos[row]=col;
             solve(row+1,n);
         }
     }
 }
-
-int main()
-{
+int main() {
     int n;
-
-    printf("Enter N: ");
+    printf("Nummber of queens: ");
     scanf("%d",&n);
-
     solve(0,n);
-
+    if(count == 0)
+        printf("No solution");
     return 0;
 }
