c5ae45baba311d57afb5b10c059a21930fd9a5fc
krishnalath <krishnalath.cs24@bmsce.ac.in>
2026-06-07 19:25:41 +0530

    Update floyd.c

--- floyd.c
+++ floyd.c
@@ -1,20 +1,15 @@
-#include<stdio.h>
-#include<limits.h>
-
+// Online C compiler to run C program online
+#include <stdio.h>
+#include <limits.h>
 int main() {
     int n;
-    printf("Enter number of vertices : ");
-    scanf("%d", &n);
-
+    printf("Enter numebr of vertices: ");
+    scanf("%d",&n);
     int a[n][n];
-    printf("Enter adjacency matrix:\n");
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < n; j++){
-            scanf("%d", &a[i][j]);
-        }
-    }
+    printf("Enter adj matrix: ");
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
+            scanf("%d",&a[i][j]);
             if(i!=j && a[i][j]==0)
                 a[i][j] = INT_MAX;
         }
@@ -22,9 +17,9 @@ int main() {
     for(int k=0;k<n;k++){
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                if(a[i][k]!=INT_MAX && a[k][j] != INT_MAX){
-                    if(a[i][k]+a[k][j] < a[i][j])
-                        a[i][j] = a[i][k] + a[k][j];
+                if(a[i][k]!=INT_MAX && a[k][j]!=INT_MAX){
+                    if(a[i][k]+a[k][j]<a[i][j])
+                        a[i][j]=a[i][k]+a[k][j];
                 }
             }
         }
@@ -33,9 +28,10 @@ int main() {
     for(int i=0;i<n;i++){
         for(int j=0;j<n;j++){
             if(a[i][j] == INT_MAX)
-                printf("INFINITY");
+                printf("INF\t");
             else
-                printf("%d",a[i][j]);
+                printf("%d\t",a[i][j]);
         }
+        printf("\n");
     }
 }
