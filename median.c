5ffd548b5fb539a21ce0b16394623832bb062987
krishnalath <krishnalath.cs24@bmsce.ac.in>
2026-06-07 23:19:16 +0530

    Create median.c

---
+++ median.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+int lomutoPartition(int arr[], int low, int high)
+{
+    int pivot = arr[low];
+    int s = low;
+
+    for(int i = low+1; i <= high; i++)
+    {
+        if(arr[i] < pivot)
+        {
+            s++;
+
+            int temp = arr[s];
+            arr[s] = arr[i];
+            arr[i] = temp;
+        }
+    }
+
+    int temp = arr[low];
+    arr[low] = arr[s];
+    arr[s] = temp;
+
+    return s;
+}
+
+int quickSelect(int arr[], int low, int high, int k)
+{
+    int s = lomutoPartition(arr, low, high);
+
+    if(s == k-1)
+        return arr[s];
+
+    else if(s > k-1)
+        return quickSelect(arr, low, s-1, k);
+
+    else
+        return quickSelect(arr, s+1, high, k);
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter number of elements: ");
+    scanf("%d",&n);
+
+    int arr[n];
+
+    printf("Enter elements: ");
+
+    for(int i=0;i<n;i++)
+        scanf("%d",&arr[i]);
+
+    int median;
+
+    if(n%2==1)
+    {
+        median = quickSelect(arr,0,n-1,(n+1)/2);
+        printf("Median = %d",median);
+    }
+    else
+    {
+        int m1 = quickSelect(arr,0,n-1,n/2);
+        int m2 = quickSelect(arr,0,n-1,n/2+1);
+
+        printf("Median = %.2f",(m1+m2)/2.0);
+    }
+
+    return 0;
+}
